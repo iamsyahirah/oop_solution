c040ed2222d13b4caa5fab8d3a47d5ef89c43c95
syahirah <iamsyahirah@users.noreply.github.com>
2023-10-06 21:51:04 +0800

    Create question_17.java

---
+++ chapter2/question_17.java
@@ -0,0 +1,19 @@
+package oop_chap2;
+
+import javax.swing.*;
+
+public class OOP_chap2 {
+
+    public static void main(String[] args) {
+        
+        JFrame myWindow;
+        
+        myWindow = new JFrame();
+        
+        myWindow.setSize(800, 600);
+        myWindow.setTitle("Welcome to Java");
+        myWindow.setVisible(true);
+        
+    }
+    
+}
